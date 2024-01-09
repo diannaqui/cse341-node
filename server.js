164c197cc91913bcd51c7d39c1c3273b43ec00e5
diannaqui <qui22001@byui.edu>
2024-01-08 17:51:54 -0700

    First commit

---
+++ server.js
@@ -0,0 +1,22 @@
+// express web server
+
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.use('/', require('./routes'))
+
+// app.listen(port, () => {
+//     console.log(`Running on port ${port}`)
+// })
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+});
